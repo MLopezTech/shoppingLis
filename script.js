8dc1e27f60916a74eb2e8fb67e1b64ce63e05a44
Miguel Lopez <chino6989@gmail.com>
2022-09-20 21:58:21 -0400

    refactored code

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
+var button = document.getElementById("enter");
 var ul = document.querySelector("ul");
 
 function inputLength(){
-    input.value.length > 0 
+    return input.value.length > 0 
 }
 
-button.addEventListener("click", function(){
-    // console.log("click is working");
-    if(input.value.length > 0 ){    
+ function createListElement(){
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
     input.value = "";
-    }
-  
-});
+ }
 
-input.addEventListener("keypress", function(event){
-    // console.log(event.keyCode);
 
-    if(input.value.length > 0 &&  event.which === 13){    
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
-    }
-  
-});
+function addEventAfterClick(){
+    if(inputLength() > 0 ){    
+        createListElement();
+         }
+}
+
+function addEventAfterKeypress(){
+    if(inputLength() > 0 &&  event.which === 13){    
+        createListElement();
+       
+     }
+}
+
+input.addEventListener("keypress", addEventAfterKeypress);
+
+button.addEventListener("click", addEventAfterClick); 
