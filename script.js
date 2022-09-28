45aef89f17d4badfc3d4cf424c8acb2b200bdd57
Miguel Lopez <chino6989@gmail.com>
2022-09-27 22:11:16 -0400

    added delete and cross out functionality

--- script.js
+++ script.js
@@ -2,17 +2,34 @@ var input = document.getElementById("userinput");
 var button = document.getElementById("enter");
 var ul = document.querySelector("ul");
 
-function inputLength(){
+
+
+// adding delete buttons to each list item
+
+// for(let i = 0; i < ul.children.length; i++){
+//     var delBtn = document.createElement("button");
+//     delBtn.classList.add("delBtn");
+//     delBtn.appendChild(document.createTextNode("del"));
+//     ul.children[i].appendChild(delBtn);
+
+// }
+
+function createListElement(){
+    var div = document.createElement("div");
+	var li = document.createElement("li");
+	var delButton = document.createElement("button");
+	div.classList.add("wrapper");
+	ul.appendChild(div);
+	div.append(li, delButton);
+	li.classList.add("taskClass");
+	li.appendChild(document.createTextNode(input.value));
+	input.value = "";
+	delButton.classList.add("delClass");
+	delButton.innerHTML='Del';
+ }
+ function inputLength(){
     return input.value.length > 0 
 }
-
- function createListElement(){
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
- }
-
 
 function addEventAfterClick(){
     if(inputLength() > 0 ){    
@@ -25,7 +42,31 @@ function addEventAfterKeypress(){
         createListElement();
        
      }
+
 }
+
+
+function doneTask(task) {
+	if (task.target.tagName === "LI"){
+		task.target.classList.toggle("done");
+	}
+}
+
+function deleteListElement(element) {
+	if (element.target.className === "delClass"){
+		element.target.parentElement.remove();
+	}
+}
+
+function handleUlClick (element) {
+	doneTask(element);
+	deleteListElement(element);
+}
+
+
+
+  
+ul.addEventListener("click", handleUlClick);
 
 input.addEventListener("keypress", addEventAfterKeypress);
 
